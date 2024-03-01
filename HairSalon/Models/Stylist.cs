6af24a239ec0611b3bc59b45b93e958a6bd45cd8
Jason Falk <jasonfalk9@gmail.com>
2024-03-01 15:37:56 -0800

    Add stylist model and db variables.

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,11 @@
+using System.Collection.Generic;
+
+namespace HairSalon.Models
+{
+  public class Stylist
+  {
+    public int StylistId { get; set; }
+    public string StylistName { get; set; }
+    public string Details { get; set; }
+  }
+}
