d44baa070b982841c91a9d76904c568d03191fe7
Jason Falk <jasonfalk9@gmail.com>
2024-03-04 14:31:34 -0800

    Separated stylist name into first and last name.

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -5,7 +5,8 @@ namespace HairSalon.Models
   public class Stylist
   {
     public int StylistId { get; set; }
-    public string StylistName { get; set; }
+    public string StylistFirstName { get; set; }
+    public string StylistLastName { get; set; }
     public string Details { get; set; }
     public List<Client> Clients { get; set; }
   }
