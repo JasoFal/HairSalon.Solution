111899bc9071fb080f9e02851ce9951bba899527
Jason Falk <jasonfalk9@gmail.com>
2024-03-04 14:34:19 -0800

    Separate client name into first and last name.

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -3,7 +3,8 @@ namespace HairSalon.Models
   public class Client
   {
     public int ClientId { get; set; }
-    public string ClientName { get; set; }
+    public string ClientFirstName { get; set; }
+    public string ClientLastName { get; set; }
     public int StylistId { get; set; }
     public Stylist Stylist { get; set; }
   }
