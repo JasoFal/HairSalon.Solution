3f8aefd42c704eef7a766e4e8ab2572730cbce5f
Jason Falk <jasonfalk9@gmail.com>
2024-04-03 11:55:30 -0700

    Add model attributes.

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -6,7 +6,9 @@ namespace HairSalon.Models
   public class Client
   {
     public int ClientId { get; set; }
+    [Required]
     public string ClientFirstName { get; set; }
+    [Required]
     public string ClientLastName { get; set; }
     public List<ClientStylist> JoinEntities { get; }
   }
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace HairSalon.Models
 {
   public class Stylist
   {
     public int StylistId { get; set; }
+    [Required]
     public string StylistFirstName { get; set; }
+    [Required]
     public string StylistLastName { get; set; }
     public string Details { get; set; }
     public List<ClientStylist> JoinEntities { get; }
